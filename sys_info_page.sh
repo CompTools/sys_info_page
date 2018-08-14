ed407b336afe1bd45ec6a99a0c83d2252d13c6b9
Matt Gitzendanner <magitz@ufl.edu>
2018-08-14 14:36:35 -0400

    End of ch 26

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -6,17 +6,26 @@ CURRENT_TIME=$(date +"%x %r %Z")
 TIME_STAMP="Generates $CURRENT_TIME, by $USER"
 
 report_uptime(){
-    echo "Function report_uptime executed"
+    cat <<- _EOF_
+        <h2>System uptime</h2>
+        <pre>$(uptime)</pre>
+_EOF_
     return
 }
 
 report_disk_space(){
-    echo "Function report_disk_space executed"
+    cat <<- _EOF_
+        <h2>Disk Space Utilization</h2>
+        <pre>$(du -sh /home/magitz/*)</pre>
+_EOF_
     return
 }
 
 report_home_space(){
-    echo "Function report_home_space executed"
+    cat <<- _EOF_
+        <h2>Home Space Utilization</h2>
+        <pre>$(du -sh /home/magitz/*)</pre>
+_EOF_
     return
 }
 
