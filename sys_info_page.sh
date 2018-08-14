9d4bcb3f080f56350d81ab9fa567a1a7e89bfe86
Matt Gitzendanner <magitz@ufl.edu>
2018-08-14 12:23:15 -0400

    Add function stubs

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -5,6 +5,18 @@ TITLE="System Information Report for $HOSTNAME"
 CURRENT_TIME=$(date +"%x %r %Z")
 TIME_STAMP="Generates $CURRENT_TIME, by $USER"
 
+report_uptime(){
+    return
+}
+
+report_disk_space(){
+    return
+}
+
+report_home_space(){
+    return
+}
+
 cat << _EOF_
 <HTML>
     <HEAD>
@@ -13,6 +25,9 @@ cat << _EOF_
     <BODY>
         <H1>$TITLE</H1>
         <p>$TIME_STAMP</p>
+        $(report_uptime)
+        $(report_disk_space)
+        $(report_home_space)
     </BODY>
 </HTML>
 _EOF_
