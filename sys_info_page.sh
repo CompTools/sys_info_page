27b77cd83088a6ee78408a53f035b61f95d2393a
Matt Gitzendanner <magitz@ufl.edu>
2018-08-14 12:16:18 -0400

    Initial commit, before converting to Here Documents

---
+++ sys_info_page.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Program to output a system information page
+
+TITLE="System Information Report for $HOSTNAME"
+CURRENT_TIME=$(date +"%x %r %Z")
+TIME_STAMP="Generates $CURRENT_TIME, by $USER"
+
+echo "<HTML>
+        <HEAD>
+            <TITLE>$TITLE</TITLE>
+        </HEAD>
+        <BODY>
+            <H1>$TITLE</H1>
+            <p>$TIME_STAMP</p>
+        </BODY>
+</HTML>"
